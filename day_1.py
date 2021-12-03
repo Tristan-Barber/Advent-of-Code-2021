fa7f3dbf4aac15c06140ab4e4e541b9803c6bbac
Tristan-Barber <worktowin1021@yahoo.com>
2021-12-02 21:15:16 -0500

    Started day 1 problem 1 and made a txt file for the depths

---
+++ day_1.py
@@ -0,0 +1,14 @@
+"""
+First solution for problem 1 of day 1 of Advent of Code
+------------------------
+Tristan Barber
+"""
+
+def depth(depth_num):
+    
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
