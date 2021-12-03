841865163afbc96c6c3b855ac8b2f96f821d2523
Tristan-Barber <worktowin1021@yahoo.com>
2021-12-03 18:39:13 -0500

    Day problem 2 not complete

--- day_1.py
+++ day_1.py
@@ -23,7 +23,7 @@ When it reaches a decreased number, it goes back to the decreased and starts the
 200  A B    
 208  A B C  
 210    B C D
-200  E   C D
+200  E   C D               
 207  E F   D
 240  E F G  
 269    F G H
@@ -43,17 +43,43 @@ H: 792 (increased)
 def easy_iterable(filename):
     depths = []
     with open(filename) as file:
-        for depth in file:
-            depths.append(depth)
-        return depths
+        return [int(i.strip()) for i in file.readlines()]
 
 def sum_comp(filename):
     increased_count = 0
     sums = []
     total = 0
     depths = easy_iterable(filename)
-    
-       
+    i = 0
+    back = None
+    back_flag = False
+    while i != len(depths):
+        current = i
+        if i-1 < 0:
+            prev = i-1
+        print(depths[current], depths[prev])
+        if increased_count == 3:
+            i-=1
+            sums.append(total)
+            total = 0
+
+        if back_flag == True:
+            back_flag = False
+            i = back
+
+        if depths[current] > depths[prev] and ((i-1 < 0) == False):
+            total += depths[current]
+            increased_count += 1
+        elif depths[current] > 0 and ((i-1 < 0) == True):
+            total += depths[current]
+            increased_count += 1
+        else:
+            total += depths[current]
+            back = current
+            back_flag = True
+            increased_count += 1
+    return sums
+
 def main():
     # print(prev_measurement_num("data/depths.txt"))
     # print(prev_measurement_num("data/test_1.txt"))
