aa8efa7c245fd5c899d2e62457d6be588477b857
Tristan-Barber <worktowin1021@yahoo.com>
2021-12-03 20:24:26 -0500

    Day 3 Problem 1 Complete

--- Day_3_Problems/day_3_problem_1.py
+++ Day_3_Problems/day_3_problem_1.py
@@ -11,6 +11,7 @@ def easy_iterable(filename):
     with open(filename) as file:
         return [i.strip() for i in file.readlines()]
 
+#I could probably use a comparitor to make it less code
 def epsilon(filename):
     #Least Common
     bits = ""
@@ -34,11 +35,30 @@ def epsilon(filename):
 def gamma(filename):
     #Most Common
     bits = ""
-    pass
+    place = 0
+    binaries = easy_iterable(filename)
+    while place != len(binaries[0]):
+        zero = 0
+        one = 0
+        for line in binaries:
+            if int(line[place]) == 0:
+                zero += 1
+            elif int(line[place]) == 1:
+                one += 1
+        if zero > one:
+            bits += "0"
+        elif one > zero:
+            bits += "1"
+        place += 1
+    return bits
+    
 
 def main():
     #print(binary_to_decimal("100101"))
-    print(epsilon("Day_3_Problems/data/day_3_input.txt"))
+    
+    gam = binary_to_decimal(gamma("Day_3_Problems/data/day_3_input.txt"))
+    eps = binary_to_decimal(epsilon("Day_3_Problems/data/day_3_input.txt"))
+    print(gam*eps)
 
 if __name__ == "__main__":
     main()
