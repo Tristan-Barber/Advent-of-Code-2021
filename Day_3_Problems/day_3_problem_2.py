e1f617e31514c4a5e6704a1b09e7403bd58b9e76
Tristan-Barber <worktowin1021@yahoo.com>
2021-12-05 09:41:10 -0500

    Positioning problem, also can't access last variable in the list

--- Day_3_Problems/day_3_problem_2.py
+++ Day_3_Problems/day_3_problem_2.py
@@ -2,8 +2,7 @@ from day_3_problem_1 import *
 
 def generator(filename):
     binaries = easy_iterable(filename)
-    place = 0
-    for place in range(12):
+    for place in range(len(binaries[0])):
         zero = 0
         one = 0
         position = 0
@@ -16,17 +15,26 @@ def generator(filename):
             if zero > one:
                 if binary[place] == "1":
                     binaries.pop(position)
+                else:
+                    position += 1
             elif one > zero:
+                print(binary[place])
                 if binary[place] == "0":
-                    binaries.pop(position)
-            position+=1
+                    print(binary)
+                    print("Place:", place)
+                    print(binary[place])
+                    print("position:",position)
+                    print(binaries[position])
+                    binaries.pop(position) 
+                position +=1
     return binaries
 
 def scrubber(filename):
     pass
 
 def main():
-    print(generator("Day_3_Problems/data/day_3_input.txt"))
+    #print(generator("Day_3_Problems/data/day_3_input.txt"))
+    print(generator("Day_3_Problems/data/test_1.txt"))
 
 if __name__ == "__main__":
     main()
