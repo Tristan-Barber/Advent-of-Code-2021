1fbf461164ea8d678831a64cd53626ca9e016816
Tristan-Barber <worktowin1021@yahoo.com>
2021-12-18 18:32:45 -0500

    Graph completed, working on file parser

--- Day_5_Problems/problem_1.py
+++ Day_5_Problems/problem_1.py
@@ -5,3 +5,24 @@ Tristan Barber
 """
 
 main_data = "Day_5_Problems/data/input.txt"
+
+#Create 2d list for the graph and start 0,0 in top left 10000 each direction
+def create_graph(size):
+    graph = [["." for _ in range(size)] for _ in range(size)]
+    return graph
+
+def arrow_parser(filename):
+    coordinated = []
+    with open(filename) as file:
+        for line in file:
+            line.strip()
+            line = line.split(" -> ")
+            line[1].replace('\n', "")
+            print(line)
+
+def main():
+    graph = create_graph(20)
+    arrow_parser(main_data)
+
+if __name__ == "__main__":
+    main()
